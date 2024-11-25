f131dfaf30dd5d27378c511063c88156ceac2e38
Borja Portero Hernandez <borjaph97@gmail.com>
2024-11-25 02:14:25 +0100

    Updated code legibility

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define MAX_LENGTH_COMMAND 50   //Command's max length in bytes
+
+// Method to read a command from user through terminal
 char* readCommand() {
     
-    char* commandToReturn= malloc(20);
+    char* commandToReturn= malloc(MAX_LENGTH_COMMAND);
     printf("BPsh>");
     scanf("%s", commandToReturn);
 
